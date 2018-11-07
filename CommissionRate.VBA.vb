80bb4aae19dafdea99f5a8d6155a50780158a222
jelockro <jlockrow@gmail.com>
2018-11-07 12:56:34 -0600

    refactored code and included commments

--- CommissionRate.VBA.vb
+++ CommissionRate.VBA.vb
@@ -1,38 +1,38 @@
-Public Function CommissionRate(age As Integer, ByRef product As Range) As String
-
+Public Function CommissionRate(age As Integer, ByRef product As Range) As Variant
+    '//// @@ <-- unsued code from previous subroutine ////
+    
+    '////  If age is 0, which occurs if the cell includes a void ////
+    If age = 0 Then CommissionRate = "Age outside the limit"
+    
     '////   initiate variables for first product test ////
     Dim rng1, rng2, rng3, offsetRange As Range
     Dim firstProductTest, secondProductTest As Range
     Dim wksSource As Worksheet
     Dim wksdest As Worksheet
 
-   
     Dim printOut As String
-    
     Dim ageRanges As Integer
     Dim address As String
     Dim productAddress As String
     
- 
     Dim forLoopRowOffsetCounter, ageMin, ageMax As Integer
     forLoopRowOffsetCounter = 0
+    Dim successfulRange As Range
     
     '////  set worksheet & range variables ////
     '@@ age = wksSource.Range("d4").Value
     '@@ modified with age input
     Set wksSource = ActiveWorkbook.Sheets(1)
     Set wksdest = ActiveWorkbook.Sheets(2)
+    
     '@@ Set rng1 = wksSource.Range("o3")
     '@@ Modified for product input
+    productAddress = product.address
     Set rng1 = wksSource.Range(productAddress)
     '@@ Set rng2 = wksdest.Range("I3:I39")
     '@@ modified with rngSellingProducts input
     Set rng2 = wksdest.Range("rngSellingProducts")
     Set rng3 = wksdest.Range("I3:I39")
-
-    productAddress = product.address
-   
-    
 
     '////   Initiate first product test ////
     Set firstProductTest = rng2.Find(rng1)
@@ -47,6 +47,7 @@ Public Function CommissionRate(age As Integer, ByRef product As Range) As String
         Debug.Print secondProductTest.address
     End If
     
+    '////  Set offset and go through AgeRange loop  ////
     Set offsetRange = wksdest.Range(address)
     ageRanges = offsetRange.Offset(1, 0)
     Debug.Print "AgeRanges: " + CStr(ageRanges)
@@ -57,15 +58,16 @@ Public Function CommissionRate(age As Integer, ByRef product As Range) As String
         ageMax = offsetRange.Offset(forLoopRowOffsetCounter, 4)
         Debug.Print "Age: " + CStr(age) + "AgeMax: " + CStr(ageMax)
         If age > ageMin And age < ageMax Then
-            Debug.Print TypeName(offsetRange.Offset(forLoopRowOffsetCounter, 5))
-            Debug.Print "the Commission Rate is :" + CStr(CommissionRate)
-            CommissionRate = CStr(CommissionRate)
+            '//// Successful search for CommissionRate ////
+            Set successfulRange = offsetRange.Offset(forLoopRowOffsetCounter, 5)
+            Debug.Print "the Commission Rate is :" + CStr(successfulRange)
+            CommissionRate = CStr(successfulRange)
             Exit For
         Else
             If i = ageRanges Then
                 printOut = "Age outside the limit"
                 Debug.Print printOut
-                josh = printOut
+                CommissionRate = printOut
             End If
             
             forLoopRowOffsetCounter = forLoopRowOffsetCounter + 1
